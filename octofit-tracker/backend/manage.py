8f2541edee85a6e850d7a0e6e759b59716498f25
umeshsureban <umesh_sv@hotmail.com>
2025-04-08 19:46:55 +0000

    third

--- octofit-tracker/backend/manage.py
+++ octofit-tracker/backend/manage.py
@@ -2,6 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management.commands.runserver import Command as runserver
+
+runserver.default_addr = '0.0.0.0'
+runserver.default_port = '8001'
 
 
 def main():
